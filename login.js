f568fbf920779cd9b60993446891a02442a09491
Machas678 <77081177+Machas678@users.noreply.github.com>
2021-04-05 11:11:07 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+function addUser() {
+
+localStorage.setItem("u_1",document.getElementById("p1-u").value);
+localStorage.setItem("u_2",document.getElementById("p2-u").value);
+window.location = ("game.html");
+
+}
